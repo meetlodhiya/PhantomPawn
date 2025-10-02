5f125d7025a5759caad40ceaa65c485f45d578b0
Meet Lodhiya <159940359+meetlodhiya@users.noreply.github.com>
2025-10-02 23:13:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+const cartographerPlugin =
+  process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
+    ? require("@replit/vite-plugin-cartographer").cartographer()
+    : null;
+
+const devBannerPlugin =
+  process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
+    ? require("@replit/vite-plugin-dev-banner").devBanner()
+    : null;
+
 export default defineConfig({
-  base: "/PhantomPawn/", // Correct base path for GitHub Pages
+  base: "/PhantomPawn/", // base path for GitHub Pages
 
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
+    ...(cartographerPlugin && devBannerPlugin ? [cartographerPlugin, devBannerPlugin] : []),
   ],
 
   root: "client",
