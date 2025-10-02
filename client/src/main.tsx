c9534a3b60060ebf48ed8e09190164b45ac9c62c
Meet Lodhiya <159940359+meetlodhiya@users.noreply.github.com>
2025-10-02 23:16:51 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { HashRouter } from "react-router-dom"; // Add this import
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter basename="/PhantomPawn">
+    <App />
+  </HashRouter>
+);
